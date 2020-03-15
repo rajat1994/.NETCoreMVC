e828b67185194045e36e71130a59c499436d99ba
rajat1994 <rajatarora21@gmail.com>
2020-03-15 19:26:14 +0530

    .net core mvc crud operations

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BookListMVC.Models;
+
+namespace BookListMVC.Controllers
+{
+    public class BooksController : Controller
+    {
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        private readonly ApplicationDbContext _db;
+
+        public BooksController(ApplicationDbContext db)
+        {
+
+            _db = db;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+
+            Book = new Book();
+            if(id == null)
+
+            {
+
+                return View(Book);
+            }
+
+            Book = _db.Books.FirstOrDefault(u => u.Id == id);
+
+            if(Book == null)
+            {
+
+                return NotFound();
+            }
+
+
+            return View(Book);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert()
+        {
+
+            if (ModelState.IsValid)
+            {
+                if(Book.Id == 0)
+                {
+                    _db.Books.Add(Book);
+                }
+
+                else
+                {
+
+
+
+                    _db.Books.Update(Book);
+                }
+
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+
+
+            return View(Book);
+        }
+
+
+        #region APICalls
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Books.ToListAsync() });
+        }
+
+
+        [HttpDelete]
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _db.Books.FirstOrDefaultAsync(u => u.Id == id);
+            if (bookFromDb == null)
+            {
+
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+
+            _db.Books.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+
+            return Json(new { success = true, message = "Delete Successfully" });
+
+
+        }
+
+
+        #endregion
+
+
+    }
+}
